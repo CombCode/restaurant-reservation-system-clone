f38ef15c38729f0d22b83054ba26328e9588d04d
marco oldani <marcoooldani@gmail.com>
2024-09-15 01:36:05 +0200

    started App.vue adaptation by removing useless things A lot of those belongs to the list view component

--- src/modules/Reservations.js
+++ src/modules/Reservations.js
@@ -10,24 +10,6 @@ export class Reservation{
         //pushToDB(this)
     }
 
-    static pushToDB(obj){
-        DB_Reservation.push(obj)   //the db is an array of objects
-    }
-    static getReservationFromDB(uid){   //TODO see if better to pass obj or uid
-        for(reservation of DB_Reservation){
-            if(reservation.uid == uid){
-                return DB_Reservation
-            }
-        }
-    }
-    static deleteFromDB(uid){
-        for(reservation of DB_Reservation){
-            if(reservation.uid == uid){
-                DB_Reservation.splice(DB_Reservation.indexOf(reservation), 1)
-            }
-        }
-    }
-
     //setters
     setName(name){
         if(stringExistAndNotEmpty(name) && name.length >= 2 && name.length <= 20){
@@ -75,32 +57,40 @@ export class Reservation{
     getSeats(uid){
 
     }
-    /* function extractDate(dateObj){
-        console.log("la data grezza è: ", this.date)
-        //parsing di Date Sat Apr 06 2024 01:14:00 GMT+0200 (Ora legale dell’Europa centrale)
-          //non necessario. calendario fornisce this.format come output della data (senza l'ora)
-        this.format = (date) => {
-          const date = date.getDate();
-          const month = date.getMonth() + 1;
-          const year = date.getFullYear();
-  
-          this.giorno.value = date + " " + month + " " + year;
-  
-  
-          return `${date}/${month}/${year}`;
+    
+    //static metods
+    static pushToDB(obj){
+        DB_Reservation.push(obj)   //the db is an array of objects
+    }
+    static getReservationFromDB(uid){   //TODO see if better to pass obj or uid
+        for(reservation of DB_Reservation){
+            if(reservation.uid == uid){
+                return DB_Reservation
+            }
         }
-      }
-  
-      function extractHour(dateObj){                        //TODO mby static getReadbleDate
-        console.log("l' ora grezza è: ", this.time)
-        //parsing di this.time
-  
-        const timeObj = JSON.parse(JSON.stringify(this.time))
-        console.log("l' ora parsata è: ",timeObj)
-  
-        this.ora.value = timeObj.hours + ":" + timeObj.minutes
-  
-      } */
+    }
+    static deleteFromDB(uid){
+        for(reservation of DB_Reservation){
+            if(reservation.uid == uid){
+                DB_Reservation.splice(DB_Reservation.indexOf(reservation), 1)
+            }
+        }
+    }
+    static getAllReservations(){
+
+    }
+    static getReservationsSortedBy(value){
+        if(value == "hour"){
+
+        }
+        else if(value == "seats"){
+
+        }
+        else{
+            
+        }
+    }
+
 }
 
 /* ---------- end of class ---------- */
